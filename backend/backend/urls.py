618954b15ceffcf51c10461cbd7efc72efdddf9e
karat <karatalleroy2308@yandex.ru>
2024-06-19 14:50:03 +0300

    переустановил докер на сервере

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,5 +8,4 @@ router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-]
+    path('api/', include(router.urls)),]
